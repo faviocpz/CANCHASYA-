12bf25572f3f925a7419439689c4d06a21ff87c4
faviocpz <103940536+faviocpz@users.noreply.github.com>
2025-04-10 15:17:16 -0500

    Inicio

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('pages/index.html')
+
+@app.route('/login')
+def login():
+    return render_template('pages/login.html')
+
+@app.route('/canchas')
+def canchas():
+    return render_template('pages/canchas.html')
+
+@app.route('/cancha')
+def cancha():
+    return render_template('pages/cancha.html')
+
+@app.route('/carrito')
+def carrito():
+    return render_template('pages/carrito.html')
+
+@app.route('/panel')
+def panel():
+    return render_template('pages/panel.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
