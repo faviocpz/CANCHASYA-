19d2be4f718a85021edc595d1bde1c1137461674
faviocpz <103940536+faviocpz@users.noreply.github.com>
2025-05-29 15:49:20 -0500

    Prueba

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ Rdashboard.registrar_rutas_dashboard(app)
 Rsuscripcion.registrar_rutas(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
